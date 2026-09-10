285a8dee6f13f31a5ebfcdbfa5beba8ad4977c1f
c2e-money <dipenbiswas9101@gmail.com>
2026-09-10 13:46:53 +0530

    Update db.ts

--- src/server/db.ts
+++ src/server/db.ts
@@ -59,7 +59,7 @@ const defaultAds: Advertisement[] = [
     id: 'ad-popunder-main',
     title: 'Popunder Ad',
     type: 'popunder',
-    code: '<script src="https://rightyrely.com/85/10/84/851084f745dc8bcc34fc5c5a02b47b70.js"></script>',
+    code: '<script src="https://rightyrely.com/53/92/fc/5392fcc75419f61c91e6f8fe414638f9.js"></script>',
     location: 'download_page',
     isEnabled: true,
     clicks: 0,
@@ -70,7 +70,7 @@ const defaultAds: Advertisement[] = [
     id: 'ad-socialbar-main',
     title: 'Social Bar Ad',
     type: 'socialbar',
-    code: '<script src="https://rightyrely.com/39/ec/5a/39ec5a182d9f0d10844597ce52e66aca.js"></script>',
+    code: '<script src="https://rightyrely.com/ae/f4/a1/aef4a178cbd7000a43b9c0e73aba7fad.js"></script>',
     location: 'download_page',
     isEnabled: true,
     clicks: 0,
@@ -81,7 +81,7 @@ const defaultAds: Advertisement[] = [
     id: 'ad-smartlink-main',
     title: 'Smart Link Monetized CTA',
     type: 'smartlink',
-    code: 'https://rightyrely.com/fak9m43i14?key=73f5ec0985cef0073e7db005b15f296d',
+    code: 'https://rightyrely.com/nvxev2d8m9?key=357f2a0b3b6161edd40942cc022bbe8a',
     location: 'download_button',
     isEnabled: true,
     clicks: 0,
@@ -94,14 +94,14 @@ const defaultAds: Advertisement[] = [
     type: 'banner',
     code: `<script>
   atOptions = {
-    'key' : 'ee10c1adfd8e4c809977c789344ef27e',
+    'key' : '6ae81df28c5e141bffdad2683ec8da66',
     'format' : 'iframe',
     'height' : 60,
     'width' : 468,
     'params' : {}
   };
 </script>
-<script src="https://rightyrely.com/ee10c1adfd8e4c809977c789344ef27e/invoke.js"></script>`,
+<script src="https://rightyrely.com/6ae81df28c5e141bffdad2683ec8da66/invoke.js"></script>`,
     location: 'download_page_top',
     isEnabled: true,
     clicks: 0,
@@ -112,8 +112,8 @@ const defaultAds: Advertisement[] = [
     id: 'ad-native-main',
     title: 'Download Page Native Banner',
     type: 'native',
-    code: `<script async="async" data-cfasync="false" src="https://rightyrely.com/326d0f3c66706f8159c1008ea05e137d/invoke.js"></script>
-<div id="container-326d0f3c66706f8159c1008ea05e137d"></div>`,
+    code: `<script async="async" data-cfasync="false" src="https://rightyrely.com/c9a50f399d991fbd28e09f98504f9cfa/invoke.js"></script>
+<div id="container-c9a50f399d991fbd28e09f98504f9cfa"></div>`,
     location: 'download_page_middle',
     isEnabled: true,
     clicks: 0,
@@ -325,3 +325,4 @@ class Database {
 }
 
 export const db = new Database();
+    
